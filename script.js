baa690470658552d7db722f4492059ac9c507991
Hyunjin Jae <jaexx003@umn.edu>
2021-04-16 19:38:35 -0500

    removed debug line

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ var apiKey = "57b8c58ea82dc7261df5a3986ec8ea6d";
 
 var link = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
 
-console.log(link);
-
 $.getJSON(link, function(data) {
     console.log(data);
 
