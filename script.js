b102be43343b5301926cea1d5ae42c2734f6193b
Hyunjin Jae <jaexx003@umn.edu>
2021-04-16 19:12:24 -0500

    update index

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var city = "Minnesota";
+var apiKey = "57b8c58ea82dc7261df5a3986ec8ea6d";
 
-var link = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid={API-KEY-HERE}";
+var link = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
+
+console.log(link);
 
 $.getJSON(link, function(data) {
     console.log(data);
